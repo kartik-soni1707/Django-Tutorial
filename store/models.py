1e0f9f8fe8b7c1db16e4ebcdbca94c67c0c23a1c
kartik-soni1707 <its.kartiksoni@gmail.com>
2024-11-30 22:46:27 -0800

    added given_name

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER,'Silver'),
         (MEMBERSHIP_GOLD,'Gold')
     ] 
-    first_name=models.CharField(max_length=120)
+    given_name=models.CharField(max_length=120)
     last_name=models.CharField(max_length=120)
     email=models.EmailField(unique=True)
     phone=models.CharField(max_length=120)
