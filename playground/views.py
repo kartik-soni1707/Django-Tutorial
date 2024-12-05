03522b99e7f3ad33f2ce22801ea4ff95ba167bd5
kartik-soni1707 <its.kartiksoni@gmail.com>
2024-12-04 20:59:24 -0800

    filtering

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product, Customer, Collection, OrderItem
 
 def say_hello(request):
-    ok=calculate()
-    return render(request,'hello.html', {'name': 'Jacob'})
-
-def calculate():
-    x=1
-    y=2
-    return x
+    query_set=OrderItem.objects.filter(product__collection__id=1)
+    return render(request,'hello.html', {'name': 'Jacob', 'result': list(query_set)})
