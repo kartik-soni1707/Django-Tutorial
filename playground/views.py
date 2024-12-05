d056cd196d4520fa30879510e31bd7d010dd65bb
kartik-soni1707 <its.kartiksoni@gmail.com>
2024-12-04 23:32:11 -0800

    more advanced filtering

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product, Customer, Collection, OrderItem
 
 def say_hello(request):
-    query_set=OrderItem.objects.filter(product__collection__id=1)
+    query_set=Product.objects.filter(id__in=OrderItem.objects.values("product_id").distinct()).order_by('title')
     return render(request,'hello.html', {'name': 'Jacob', 'result': list(query_set)})
